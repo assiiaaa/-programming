754dc6d88530f9ae9e716300fbd7e0188ed33142
assiiaaa <144883293+assiiaaa@users.noreply.github.com>
2024-03-15 09:37:10 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class ArrayManipulator
+{
+    private Random random = new Random();
+
+    public int[] GenerateRandomArray(int length, int min, int max)
+    {
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.Next(min, max + 1);
+        }
+        return array;
+    }
+
+    public int FindMax(int[] array)
+    {
+        int max = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayManipulator manipulator = new ArrayManipulator();
+
+
+        int[] array = manipulator.GenerateRandomArray(10, 1, 100);
+        Console.WriteLine("Generated Array:");
+        PrintArray(array);
+
+
+        int max = manipulator.FindMax(array);
+        Console.WriteLine("\nMaximum Element: " + max);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
